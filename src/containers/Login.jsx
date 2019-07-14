5b097966e4efca4be33731a719cddd6cd818de80
Heather You <heather.fswd@gmail.com>
2019-07-14 13:19:31 -0400

    Remove link opening in new tab

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -7,7 +7,7 @@ export default class Login extends Component {
 
     return (
       <div className="Login">
-        <a href={spotifyFunctions.getRedirectUrl()} target="_blank" rel="noopener noreferrer">
+        <a href={spotifyFunctions.getRedirectUrl()}>
           <button id="btn-spotify">Log in to Spotify</button>
         </a>
       </div>
