4ff5040880946248eb5559d0fe33450b2265f99f
Heather You <heatherhyou@gmail.com>
2021-03-23 22:51:14 -0400

    refactor: convert SetlistView component from class to functional

--- src/containers/SearchResults/SetlistView.jsx
+++ src/containers/SearchResults/SetlistView.jsx
@@ -1,33 +1,37 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-wrap-multilines */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import TitleForm from './TitleForm';
 import Track from './Track';
+import PropTypes from 'prop-types';
 
-export default class SetlistView extends Component {
-  render() {
-    const {
-      playlistTracks, saveTitleHandler, setlist, title, handleAddTrack, handleRemoveTrack,
-    } = this.props;
+export const SetlistView = ({ playlistTracks, saveTitleHandler, setlist, title, handleAddTrack, handleRemoveTrack }) => {
+  return (
+    <div id="SetlistView" >
+      <TitleForm title={title} saveTitleHandler={saveTitleHandler} />
 
-    return (
-      <div id="SetlistView">
-        <TitleForm title={title} saveTitleHandler={saveTitleHandler} />
-
-        { setlist.tracks
-          && <div className="App-list">
-            {setlist.tracks.map((track, id) => <Track
-              key={id}
-              playlistTracks={playlistTracks}
-              title={track.name}
-              uri={track.trackUri}
-              id={id}
-              addTrack={handleAddTrack}
-              removeTrack={handleRemoveTrack}
-            />)}
-          </div>}
-      </div>
-    );
-  }
+      { setlist.tracks
+        && <div className="App-list">
+          {setlist.tracks.map((track, id) => <Track
+            key={id}
+            playlistTracks={playlistTracks}
+            title={track.name}
+            uri={track.trackUri}
+            id={id}
+            addTrack={handleAddTrack}
+            removeTrack={handleRemoveTrack}
+          />)}
+        </div>
+      }
+    </div >
+  );
 }
+
+SetlistView.propTypes = {
+  playlistTracks: PropTypes.array,
+  saveTitleHandler: PropTypes.func,
+  setlist: PropTypes.object,
+  title: PropTypes.string,
+  handleAddTrack: PropTypes.func,
+  handleRemoveTrack: PropTypes.func,
+}
+
+export default SetlistView;
