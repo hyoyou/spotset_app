28d8f735d4ec120fd2a37d8434a3f8c2fbb0515c
Heather You <heatherhyou@gmail.com>
2021-03-23 23:47:48 -0400

    refactor: convert Setlists component from class to functional

--- src/containers/SearchForm/Setlists.jsx
+++ src/containers/SearchForm/Setlists.jsx
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default class Setlists extends Component {
-  state = {
-    setlistId: '',
+export const Setlists = ({ onClick }) => {
+  const [setlistId, setSetlistId] = useState('');
+
+  const handleChange = (event) => {
+    setSetlistId(event.target.value);
   }
 
-  onInput = (event) => {
-    this.setState({
-      setlistId: event.target.value
-    })
-  }
-
-  onClick = (event) => {
+  const handleClick = (event) => {
     event.preventDefault();
-    this.props.onClick(this.state.setlistId);
+    onClick(setlistId);
   }
 
-  render() {
-    const { setlistId } = this.state;
+  return (
+    <div className='App-field'>
+      <form onSubmit={handleClick}>
+        <input id='setlistId' name='setlistId' type='text' value={setlistId} onChange={handleChange} placeholder='Enter setlist ID' />
 
-    return (
-      <div className='App-field'>
-        <form onSubmit={ this.onClick }>
-          <input id='setlistId' name='setlistId' type='text' value={ setlistId } onChange={ this.onInput } placeholder='Enter setlist ID' />
-          
-          <input id='btn-setlistId' type='submit' value='Display Setlist' onClick={this.onClick} />
-        </form>
+        <input id='btn-setlistId' type='submit' value='Display Setlist' onClick={handleClick} />
+      </form>
 
-        <div id='instructions'>
-          <p>Search for your favorite artist and performance from <a href='https://www.setlist.fm/' target="_blank" rel="noopener noreferrer">Setlist.fm</a></p>
-          <p>Find the Setlist ID from the URL of your chosen performance like below and enter in the field above</p>
-          <img src='https://spotset.s3.amazonaws.com/Screen+Shot+2019-07-18+at+12.54.38+AM.png' alt='location of setlist id' />
-        </div>
-
-        <p><br /><br />Search for setlists by artist coming soon!</p>
+      <div id='instructions'>
+        <p>Search for your favorite artist and performance from <a href='https://www.setlist.fm/' target="_blank" rel="noopener noreferrer">Setlist.fm</a></p>
+        <p>Find the Setlist ID from the URL of your chosen performance like below and enter in the field above</p>
+        <img src='https://spotset.s3.amazonaws.com/Screen+Shot+2019-07-18+at+12.54.38+AM.png' alt='location of setlist id' />
       </div>
-    )
-  }
+
+      <p><br /><br />Search for setlists by artist coming soon!</p>
+    </div>
+  )
 }
+
+Setlists.propTypes = {
+  onClick: PropTypes.func
+}
+
+Setlists.defaultProps = {
+  onClick: () => { }
+}
+
+export default Setlists;
--- src/tests/containers/SearchForm/Setlists.test.js
+++ src/tests/containers/SearchForm/Setlists.test.js
@@ -1,27 +1,47 @@
 /* eslint-disable no-undef */
 import { mount } from 'enzyme';
-import React from 'react';
+import React, { useState as useStateMock } from 'react';
 import Setlists from '../../../containers/SearchForm/Setlists';
 
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
 describe('Setlists Component', () => {
-  it('renders an input field for the setlistId that updates the state when changed', () => {
+  const setState = jest.fn();
+
+  beforeEach(() => {
+    useStateMock.mockImplementation(init => [init, setState]);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders an input field for the setlistId with a default value', () => {
     const wrapper = mount(<Setlists />);
-
-    expect(wrapper.state().setlistId).toEqual('');
-
     const input = wrapper.find('#setlistId');
-    input.instance().value = 'abc1234';
-    input.simulate('change');
-    expect(wrapper.instance().state.setlistId).toEqual('abc1234');
+
+    expect(input.props().value).toEqual('');
+  });
+
+  it('updates setlistId state with value in input field', () => {
+    const wrapper = mount(<Setlists />);
+    const input = wrapper.find('#setlistId');
+
+    input.simulate('change', { target: { value: 'abc1234' } });
+
+    expect(setState).toHaveBeenCalledWith('abc1234');
   });
 
   it('callback function is called when display button is clicked', () => {
     const onClick = jest.fn();
-
     const wrapper = mount(<Setlists onClick={onClick} />);
-
     const button = wrapper.find('#btn-setlistId').at(0);
+
     button.simulate('click');
+
     expect(onClick).toHaveBeenCalled();
   });
 });
